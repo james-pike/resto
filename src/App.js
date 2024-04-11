550ddab60ef0fd507fd0f2aee6498fcdfe689f56
James <jamesandrewpike@gmail.com>
2024-04-11 16:43:30 -0400

    Add mobile conditional

--- src/App.js
+++ src/App.js
@@ -5,46 +5,50 @@ import Navbar from './partials/Navbar';
 function App() {
   return (
     <div>
-    <div className='overflow-x-hidden hidden md:block'>
-      <Navbar/>
-      {/* background */}
-      <video src={process.env.PUBLIC_URL + "/assets/video/almonte1.mp4"} loop autoPlay muted className='object-cover absolute h-screen w-screen -z-10 top-0 left-0'></video>
+      {/* For desktop (medium screens and above) */}
+      <div className='overflow-x-hidden hidden md:block'>
+        <Navbar/>
+        {/* background */}
+        <video src={process.env.PUBLIC_URL + "/assets/video/almonte1.mp4"} loop autoPlay muted className='object-cover absolute h-screen w-screen -z-10 top-0 left-0'></video>
 
-      {/* content */}
-      <div className='px-72 w-[80%] h-screen flex flex-col justify-center text-white space-y-5'>
-        <span className='text-6xl'>
-          Background Video
-        </span>
-        <span className='text-neutral-200'>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, quas, iusto corrupti quidem, molestiae id quia necessitatibus veritatis dolorum reprehenderit totam eligendi aliquid quo suscipit praesentium commodi optio! Animi eaque omnis, velit et alias inventore maxime mollitia accusamus laborum rem! Est dolor veniam hic ad eos illum similique maxime harum, eveniet eum in neque quidem magnam consectetur facere tempora animi error repellat aliquid sequi praesentium! Quidem nisi, perferendis blanditiis quam placeat voluptate optio provident eveniet voluptatum modi sequi dolorum repellat ipsam doloremque consectetur, autem voluptas fugiat reiciendis ullam ipsum eum! Excepturi nesciunt et facere. Voluptas error quam excepturi expedita quod!
-        </span>
-        <button className='w-fit px-12 py-4 bg-black border-2 border-black text-white hover:bg-transparent hover:border-white duration-200'>
-          Read More
-        </button>
+        {/* content */}
+        <div className='px-72 w-[80%] h-screen flex flex-col justify-center text-white space-y-5'>
+          <span className='text-6xl'>
+            Background Video
+          </span>
+          <span className='text-neutral-200'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, quas, iusto corrupti quidem, molestiae id quia necessitatibus veritatis dolorum reprehenderit totam eligendi aliquid quo suscipit praesentium commodi optio! Animi eaque omnis, velit et alias inventore maxime mollitia accusamus laborum rem! Est dolor veniam hic ad eos illum similique maxime harum, eveniet eum in neque quidem magnam consectetur facere tempora animi error repellat aliquid sequi praesentium! Quidem nisi, perferendis blanditiis quam placeat voluptate optio provident eveniet voluptatum modi sequi dolorum repellat ipsam doloremque consectetur, autem voluptas fugiat reiciendis ullam ipsum eum! Excepturi nesciunt et facere. Voluptas error quam excepturi expedita quod!
+          </span>
+          <button className='w-fit px-12 py-4 bg-black border-2 border-black text-white hover:bg-transparent hover:border-white duration-200'>
+            Read More
+          </button>
+        </div>
       </div>
-    </div>
 
-    <div className='overflow-x-hidden block md:hidden'>
-      <Navbar/>
-      {/* background */}
-      {/* <video src={process.env.PUBLIC_URL + "/assets/video/almonte1.mp4"} loop autoPlay muted className='object-cover absolute h-screen w-screen -z-10 top-0 left-0'></video> */}
+      {/* For mobile (small screens) */}
+      <div className='overflow-x-hidden block md:hidden'>
+        <Navbar/>
+        {/* background */}
+        <div
+          className='bg-center bg-cover absolute h-screen w-screen -z-10 top-0 left-0'
+          style={{
+            backgroundImage: `url(${process.env.PUBLIC_URL}/assets/images/background.jpg)`,
+          }}
+        ></div>
 
-      {/* content */}
-      <div className='px-72 w-[80%] h-screen flex flex-col justify-center text-white space-y-5'>
-        <span className='text-6xl'>
-          Background Video
-        </span>
-        <span className='text-neutral-200'>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, quas, iusto corrupti quidem, molestiae id quia necessitatibus veritatis dolorum reprehenderit totam eligendi aliquid quo suscipit praesentium commodi optio! Animi eaque omnis, velit et alias inventore maxime mollitia accusamus laborum rem! Est dolor veniam hic ad eos illum similique maxime harum, eveniet eum in neque quidem magnam consectetur facere tempora animi error repellat aliquid sequi praesentium! Quidem nisi, perferendis blanditiis quam placeat voluptate optio provident eveniet voluptatum modi sequi dolorum repellat ipsam doloremque consectetur, autem voluptas fugiat reiciendis ullam ipsum eum! Excepturi nesciunt et facere. Voluptas error quam excepturi expedita quod!
-        </span>
-        <button className='w-fit px-12 py-4 bg-black border-2 border-black text-white hover:bg-transparent hover:border-white duration-200'>
-          Read More
-        </button>
+        {/* content */}
+        <div className='px-72 w-[80%] h-screen flex flex-col justify-center text-white space-y-5'>
+          <span className='text-6xl'>
+            Background Image
+          </span>
+          <span className='text-neutral-200'>
+            This is the content for mobile with background image.
+          </span>
+          <button className='w-fit px-12 py-4 bg-black border-2 border-black text-white hover:bg-transparent hover:border-white duration-200'>
+            Read More
+          </button>
+        </div>
       </div>
-    </div>
-
-
-
     </div>
   );
 }
