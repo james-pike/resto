eaa5e67b57febf841bb08074a3e46d8eed28a283
James <jamesandrewpike@gmail.com>
2024-04-11 17:21:41 -0400

    Add mobile conditional

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
                 BgVideo
             </span>
             <ul className="flex h-full space-x-4">
-                <li className="cursor-pointer hover:text-purple-500 duration-200">
+                {/* <li className="cursor-pointer hover:text-purple-500 duration-200">
                     Wellington
                 </li>
                 <li className="cursor-pointer hover:text-purple-500 duration-200">
@@ -15,16 +15,23 @@ const Navbar = () => {
                 </li>
                 <li className="cursor-pointer hover:text-purple-500 duration-200">
                     Almonte
+                </li> */}
+                {/* <li>
+                    <div className="w-[1px] h-full bg-white"></div>
+                </li> */}
+                <li className="cursor-pointer hover:text-purple-500 duration-200">
+                    <a href="https://booking.resdiary.com/widget/Standard/JoesItalianKitchenWellington/438">Reservations</a>
                 </li>
                 <li>
                     <div className="w-[1px] h-full bg-white"></div>
                 </li>
                 <li className="cursor-pointer hover:text-purple-500 duration-200">
-                    Reservations
+                    <a href="https://joesitaliankitchen-1asc.mobi2go.com/">Order Online</a>
                 </li>
             </ul>
         </div>
-     );
+    );
+    
 }
  
 export default Navbar;
